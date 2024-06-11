7c194e3f191d226de100c8b9f33bd0d88d4a2c04
Eiman <eabdelmoneim@gmail.com>
2024-06-11 15:24:27 -0500

    added comments

--- smart-wallets-multiadmin.ts
+++ smart-wallets-multiadmin.ts
@@ -33,12 +33,14 @@ const smartAccount = await wallet.connect({
 });
 console.log("connected smart Account without override: " + smartAccount.address);
 
+// get AccountFactory contract
 const contract = getContract({
   client,
   chain: arbitrumSepolia,
   address: "0xCE7Ca70f626730B4E2E1BC56B55fc5347a9e8235",
 });
 
+// query Account Factory for all the smart accounts that have the wallet as a signer
 const addresses = await getAccountsOfSigner({
   contract: contract,
   signer: eoaAccount.address,
@@ -46,6 +48,8 @@ const addresses = await getAccountsOfSigner({
 
 console.log("signer accounts addresses: " + addresses);
 
+// connect to the first smart account in the list by passing in an override for the account address
+// if no override is passed, the smart account that will be connected will be the deterministic default for that signer
 const wallet2 = smartWallet({
   factoryAddress: "0xCE7Ca70f626730B4E2E1BC56B55fc5347a9e8235",
   chain: arbitrumSepolia,
